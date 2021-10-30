712b5eb10057a8ff4a3fc64bb60b8a50e55b9e33
Dup4 <lyuzhi.pan@gmail.com>
2021-10-30 17:15:21 +0800

    feat: zjcpc v2

---
+++ zjcpc/v2/sync.py
@@ -0,0 +1,129 @@
+from os import path
+import os
+import json
+import time
+import requests
+# PyExecJS
+import execjs
+
+
+def json_output(data):
+    return json.dumps(data, sort_keys=False, separators=(',', ':'), ensure_ascii=False)
+
+
+def json_input(path):
+    with open(path, 'r') as f:
+        return json.load(f)
+
+
+def mkdir(_path):
+    if not path.exists(_path):
+        os.makedirs(_path)
+
+
+def get_timestamp(dt):
+    # 转换成时间数组
+    timeArray = time.strptime(dt, "%Y-%m-%d %H:%M:%S")
+    # 转换成时间戳
+    timestamp = time.mktime(timeArray)
+    return int(timestamp)
+
+
+def output(filename, data):
+    with open(path.join(data_dir, filename), 'w') as f:
+        f.write(json_output(data))
+
+
+def get_now():
+    return int(round(time.time() * 1000))
+
+
+_params = json_input('params.json')
+data_dir = _params['data_dir']
+
+
+def fetch(key):
+    key_url = key + "_url"
+    if key_url in _params.keys():
+        url = _params[key_url]
+        params = (
+            ('t', get_now()),
+        )
+        response = requests.get(url, params=params)
+
+        text = response.text.lstrip("var {} =".format(key)).rstrip(";\n ")
+        text = "JSON.stringify(" + text + ")"
+        text = execjs.eval(text)
+
+        return json.loads(text)
+    else:
+        key_file = key + "_file"
+        board_file = _params[key_file]
+        return json_input(board_file)
+
+
+def fetch_all():
+    return fetch("teams"), fetch("runs")
+
+
+def team_output(teams):
+    print(teams)
+    team_dic = teams
+    team = {}
+    for key in team_dic:
+        item = team_dic[key]
+        team[key] = {}
+        new_item = team[key]
+        new_item['organization'] = item['school']
+        new_item['name'] = item['team']
+        members = item['members'].split('、')
+        members.sort()
+        new_item['members'] = members
+        type = item['type'].split(" ")
+        if 'unofficial' in type:
+            new_item['unofficial'] = 1
+        else:
+            for tp in type:
+                if tp == 'type1':
+                    new_item['official'] = 1
+
+    output("team.json", team)
+
+
+def run_output(runs):
+    print(runs)
+    run_list = runs
+    run = []
+    for item in run_list:
+        new_item = {}
+        new_item['team_id'] = item[0]
+        new_item['problem_id'] = ord(item[1]) - ord('A')
+        new_item['timestamp'] = (int(item[2] // 1000) // 60) * 60
+        status = item[3]
+        if status == 'AC':
+            new_item['status'] = 'correct'
+        elif status == 'NO':
+            new_item['status'] = 'incorrect'
+        elif status == 'NEW':
+            new_item['status'] = 'pending'
+        run.append(new_item)
+
+    output("run.json", run)
+
+
+def sync():
+    while True:
+        print("fetching...")
+        try:
+            teams, runs = fetch_all()
+            team_output(teams)
+            run_output(runs)
+            print("fetch successfully")
+        except Exception as e:
+            print("fetch failed...")
+            print(e)
+        print("sleeping...")
+        time.sleep(20)
+
+
+sync()
