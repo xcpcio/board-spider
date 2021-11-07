cf083bd7cffb4b347790b2f8b1c9ac3b4288ce4b
Dup4 <lyuzhi.pan@gmail.com>
2021-11-07 15:40:07 +0800

    fix: default balloon

--- generate-config/gen_config.py
+++ generate-config/gen_config.py
@@ -39,23 +39,24 @@ def generate_problem_label(num):
 
 
 def generate_balloon_color(num):
-    default_balloon_color = [
-        {'background_color': 'rgba(189, 14, 14, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(255, 144, 228, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(255, 255, 255, 0.7)', 'color': '#000'},
-        {'background_color': 'rgba(38, 185, 60, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(239, 217, 9, 0.7)', 'color': '#000'},
-        {'background_color': 'rgba(243, 88, 20, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(12, 76, 138, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(156, 155, 155, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(4, 154, 115, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(159, 19, 236, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(42, 197, 202, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(142, 56, 54, 0.7)', 'color': '#fff'},
-        {'background_color': 'rgba(0, 0, 0, 0.7)', 'color': '#fff'},
-    ]
-
     return default_balloon_color[:num]
+
+
+default_balloon_color = [
+    {'background_color': 'rgba(189, 14, 14, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(255, 144, 228, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(255, 255, 255, 0.7)', 'color': '#000'},
+    {'background_color': 'rgba(38, 185, 60, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(239, 217, 9, 0.7)', 'color': '#000'},
+    {'background_color': 'rgba(243, 88, 20, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(12, 76, 138, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(156, 155, 155, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(4, 154, 115, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(159, 19, 236, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(42, 197, 202, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(142, 56, 54, 0.7)', 'color': '#fff'},
+    {'background_color': 'rgba(0, 0, 0, 0.7)', 'color': '#fff'},
+]
 
 
 data_dir = "./data"
