8b3f13fba2b2f9e3147a09b67bf666eae052f2b6
Dup4 <lyuzhi.pan@gmail.com>
2022-05-27 17:45:01 +0800

    chore: load nvm

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -2,6 +2,10 @@
 
 set -e -x
 
+NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+export NVM_DIR
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 if [ X"${1}" = X"primary" ]; then
     cd /root/board-spider
     exec python3 sync.py
