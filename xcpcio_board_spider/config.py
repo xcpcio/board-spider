5c998f6db85d4b1dfd7a970b39efbd6a958d0bbf
Dup4 <lyuzhi.pan@gmail.com>
2023-04-30 23:27:14 +0800

    feat: Submission and Team definition
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- xcpcio_board_spider/config.py
+++ xcpcio_board_spider/config.py
@@ -1,4 +1,3 @@
-
 import typing
 
 
@@ -40,3 +39,38 @@ class Contest:
         self.medal = medal
         self.balloon_color = [Color(**item) for item in balloon_color]
         self.logo = Image(**logo)
+
+
+class Submission:
+    def __init__(self,
+                 team_id: str,
+                 problem_id: int,
+                 timestamp: int,
+                 status: str,
+                 submission_id: str = None):
+        self.team_id = team_id
+        self.problem_id = problem_id
+        self.timestamp = timestamp
+        self.status = status
+        self.submission_id = submission_id
+
+
+class Team:
+    def __init__(self,
+                 team_id: str,
+                 name: str,
+                 organization: str,
+                 members: typing.List[str],
+                 coach: str,
+                 official: bool = False,
+                 unofficial: bool = False,
+                 girl: bool = False):
+        self.team_id = team_id
+        self.name = name
+        self.organization = organization
+        self.members = members
+        self.coach = coach
+
+        self.official = official
+        self.unofficial = unofficial
+        self.girl = girl
