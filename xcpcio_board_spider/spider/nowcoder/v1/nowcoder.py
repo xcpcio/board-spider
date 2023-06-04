c073a258a316812e44a563cf1062baf7c2c24097
Dup4 <lyuzhi.pan@gmail.com>
2023-06-04 19:33:59 +0800

    feat: fetch_submissions for nowcoder spider
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- xcpcio_board_spider/spider/nowcoder/v1/nowcoder.py
+++ xcpcio_board_spider/spider/nowcoder/v1/nowcoder.py
@@ -3,20 +3,19 @@ from xcpcio_board_spider.type import Contest, Team, Teams, Submission, Submissio
 
 import json
 import requests
-import concurrent.futures
 
 
 class NowCoder:
-    def __init__(self, contest: Contest, contest_id: int, fetch_uri: str = None):
+    CONSTANT_RANK_URL = "https://ac.nowcoder.com/acm-heavy/acm/contest/real-time-rank-data"
+    CONSTANT_SUBMISSIONS_URL = "https://ac.nowcoder.com/acm-heavy/acm/contest/status-list"
+
+    def __init__(self, contest: Contest, contest_id: int):
         self.contest = contest
         self.contest_id = contest_id
 
-        if fetch_uri is None:
-            self.fetch_uri = "https://ac.nowcoder.com/acm-heavy/acm/contest/real-time-rank-data"
-        else:
-            self.fetch_uri = fetch_uri
-
         self.fetch_res_list = []
+
+        self.logger = utils.init_logger()
 
         self.teams = Teams()
         self.submissions = Submissions()
@@ -36,7 +35,7 @@ class NowCoder:
         )
 
         resp = requests.get(
-            self.fetch_uri, headers=headers, params=params)
+            self.CONSTANT_RANK_URL, headers=headers, params=params)
         if resp.status_code != 200:
             raise RuntimeError(
                 "fetch rank failed. [status_code={}]".format(resp.status_code))
@@ -58,7 +57,8 @@ class NowCoder:
                 ('page', str(i)),
             )
 
-            resp = requests.get(self.fetch_uri, headers=headers, params=params)
+            resp = requests.get(self.CONSTANT_RANK_URL,
+                                headers=headers, params=params)
             if resp.status_code != 200:
                 raise RuntimeError(
                     "fetch rank failed. [status_code={}]".format(resp.status_code))
@@ -74,6 +74,81 @@ class NowCoder:
 
         return self
 
+    def fetch_single_team_submissions(self, team: Team):
+        runs = Submissions()
+
+        page_ix = 1
+
+        while True:
+            params = (
+                ('token', ''),
+                ('id', self.contest_id),
+                ('page', page_ix),
+                ('pageSize', 50),
+                ('searchUserName', team.name),
+                ('_', utils.get_now_timestamp_second()),
+            )
+            headers = {}
+
+            resp = requests.get(self.CONSTANT_SUBMISSIONS_URL,
+                                headers=headers, params=params)
+            if resp.status_code != 200:
+                raise RuntimeError(
+                    "fetch rank failed. [status_code={}]".format(resp.status_code))
+
+            res = json.loads(resp.content)
+            if res["code"] != 0:
+                raise RuntimeError(
+                    "fetch rank failed. [code={}]".format(res["code"]))
+
+            res = res["data"]
+
+            for r in res["data"]:
+                run = Submission()
+
+                team_name = r["userName"]
+                if team_name != team.name:
+                    continue
+
+                timestamp = (int(r["submitTime"]) // 1000)
+                if timestamp > self.contest.end_time:
+                    continue
+
+                status = r["statusMessage"]
+
+                if status == "编译错误":
+                    continue
+
+                run.submission_id = str(r["submissionId"])
+                run.timestamp = timestamp - self.contest.start_time
+                run.team_id = str(r["userId"])
+                run.problem_id = ord(str(r["index"])) - ord("A")
+
+                if status == "答案正确":
+                    run.status = constants.RESULT_CORRECT
+                else:
+                    run.status = constants.RESULT_INCORRECT
+
+                runs.append(run)
+
+            if page_ix == int(res["basicInfo"]["pageCount"]):
+                break
+
+            page_ix += 1
+
+        return runs
+
+    def fetch_submissions(self):
+        runs = Submissions()
+        for t in self.teams.values():
+            self.logger.info("fetch submissions. [team={}]".format(t.name))
+
+            r = self.fetch_single_team_submissions(t)
+            runs.extend(r)
+
+        self.runs = runs
+        return self
+
     def parse_teams(self):
         teams = Teams()
 
@@ -81,7 +156,7 @@ class NowCoder:
             item = res['data']
 
             for raw_team in item['rankData']:
-                team_id = raw_team['uid']
+                team_id = str(raw_team['uid'])
                 team_name = raw_team['userName'].strip()
                 team_organization = '---'
 
