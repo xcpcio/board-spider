272f214dd0a3bd9fa2b0e26cb6f340aa60177d26
Dup4 <lyuzhi.pan@gmail.com>
2023-06-04 14:27:30 +0800

    chore: add ENABLE_REMOTE_TEST env var
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- tests/spider/nowcoder/v1/nowcoder_test.py
+++ tests/spider/nowcoder/v1/nowcoder_test.py
@@ -8,11 +8,12 @@ current_dir_path = os.path.dirname(current_file_path)
 
 
 def test_spider_nowcoder_v1_2023_gxcpc_warmup(snapshot):
-    test_prefix = "2023_gxcpc_warmup"
-
     c = Contest()
     c.start_time = utils.get_timestamp_second("2023-06-03 15:00:00")
     c.end_time = utils.get_timestamp_second("2023-06-03 17:00:00")
+
+    if os.getenv("ENABLE_REMOTE_TEST") != "true":
+        return
 
     n = NowCoder(c, 59039)
 
