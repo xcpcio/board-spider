c5547f5d1ce2b00cb39dccb0bed38f9c5e1bde3c
Dup4 <lyuzhi.pan@gmail.com>
2023-10-24 08:49:16 +0800

    style: format
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -4,6 +4,7 @@ set -e -x
 
 NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 export NVM_DIR
+# shellcheck disable=SC1091
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 if [ X"${1}" = X"primary" ]; then
