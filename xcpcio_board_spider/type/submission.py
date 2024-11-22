b8ef8731eb2b89e71bb345a3a35e8a8db9a6f3b6
Duplicate4 <lyuzhi.pan@gmail.com>
2024-11-22 08:41:28 +0800

    feat: introduce reaction video (#37)
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- xcpcio_board_spider/type/submission.py
+++ xcpcio_board_spider/type/submission.py
@@ -1,5 +1,7 @@
-import typing
 import json
+from typing import List, Optional
+
+from .type import Reaction
 
 
 class Submission:
@@ -8,9 +10,10 @@ class Submission:
                  team_id: str = "",
                  problem_id: int = 0,
                  timestamp: int = 0,
-                 time: int = None,
-                 language: str = None,
-                 submission_id: str = None):
+                 time: Optional[int] = None,
+                 language: Optional[str] = None,
+                 submission_id: Optional[str] = None,
+                 reaction: Optional[Reaction] = None):
         self.status = status
         self.team_id = team_id
         self.problem_id = problem_id
@@ -19,6 +22,7 @@ class Submission:
         self.time = time
         self.language = language
         self.submission_id = submission_id
+        self.reaction = reaction
 
     @property
     def get_dict(self):
@@ -38,6 +42,9 @@ class Submission:
         if self.submission_id is not None:
             obj["submission_id"] = self.submission_id
 
+        if self.reaction is not None:
+            obj["reaction"] = self.reaction.get_dict
+
         return obj
 
     @property
@@ -45,7 +52,7 @@ class Submission:
         return json.dumps(self.get_dict)
 
 
-ISubmissions = typing.List[Submission]
+ISubmissions = List[Submission]
 
 
 class Submissions(ISubmissions):
